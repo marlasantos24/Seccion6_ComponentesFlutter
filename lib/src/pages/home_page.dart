9128ab66b295c69431efbdcd18d2fb429b9a5e1d
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-02-18 21:00:26 -0500

    HomePague_FutureBuild

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -12,21 +12,34 @@ class HomePage extends StatelessWidget {
       body: _lista(),
     );
   }
-   Widget _lista() {
-     print (menuProvider.opciones);
-     return ListView(
-       children: _listaItems(),
-     );
+  Widget _lista() {
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data, context),
+        );
+      },
+    );
   }
 
-  List<Widget> _listaItems() {
-
-    return[
-      ListTile( title: Text('Hola Mundo')),
-      Divider(),
-      ListTile( title: Text('Hola Mundo')),
-      Divider(),
-      ListTile( title: Text('Hola Mundo')),
-    ]; 
+  List<Widget> _listaItems(data, context) {
+    final List<Widget> opciones = [];
+    if (data == null) {
+      return [];
+    }
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.account_circle, color: Colors.blue),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+      opciones
+        ..add(widgetTemp)
+        ..add(Divider());
+    });
+    return opciones;
   }
 }
