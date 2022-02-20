b9a08854c730bff94ba573cb850ba1c48d736f39
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-02-20 09:11:01 -0500

    input_Fields

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/alert_page.dart';
@@ -14,6 +15,7 @@ Map <String, WidgetBuilder> getApplicationRoutes(){
         'avatar' : (BuildContext context) => AvatarPage(),
         'card' : (BuildContext context) => CardPage(),
         'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
+        'inputs' : (BuildContext context) => InputPage(),
       };
 }
 
