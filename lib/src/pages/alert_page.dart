8556e14dd42bfed70f1ae406eea984d21d07f985
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-02-19 21:43:27 -0500

    AlertPage_WidgetAlert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,16 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Mostrar Alerta'),
+          onPressed: () => _mostrarAlert(context),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            shape: StadiumBorder()
+          ),
+          ),
+        ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_location),
         onPressed: (){
@@ -16,5 +26,35 @@ class AlertPage extends StatelessWidget {
         },
     )
     );
+  }
+
+  _mostrarAlert(BuildContext context) {
+    showDialog(
+      context: context, 
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la caja de la alerta'),
+              FlutterLogo(size: 100.0,)
+            ]
+            ),
+            actions: <Widget>[
+             TextButton(
+             child: Text('Cancelar'),
+             onPressed: () => Navigator.of(context).pop(),
+             ),
+             TextButton(
+             child: Text('Ok'),
+             onPressed: () => Navigator.of(context).pop(),
+             )
+            ],
+        );
+      }
+      );
   }
 }
